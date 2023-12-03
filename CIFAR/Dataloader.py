644641edad946469e794aae27e75a033154e4a99
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2023-12-03 20:36:53 +0400

    Update Dataloader.py

--- CIFAR/Dataloader.py
+++ CIFAR/Dataloader.py
@@ -1,7 +1,11 @@
+import os
+
 import torch
 import torch.nn as nn
 import torchvision
 import torch.utils.data
+from torchvision import transforms
+
 
 
 
@@ -23,3 +27,34 @@ class DatasetTransformer(torch.utils.data.Dataset):
 
 
 def get_dataloaders(config, use_cuda):
+    """
+        Arguments :
+        ratio       : float in ]0,1[   , pourcentage de donnees de train qui vont etre utilisees pour la validation
+        batch_size  : integer          , taille des batchs
+        num_workers : integer          , nombre de processus paralleles pour charger les donnees
+        use_cuda    : Boolean          , Utilisation d'un gpu
+        data_dir    :
+    """
+
+    ratio = config["data"]["valid_ratio"]
+    batch_size = config["data"]["batch_size"]
+    num_workers = config["data"]["num_workers"]
+    data_dir = config["data"]["trainpath"]
+
+    if config["data"]["dataset"] in ["CIFAR10", "CIFAR100"]:
+        dataset = eval(
+            f"torchvision.datasets.{config['data']['dataset']}(root=data_dir, train=True, download=True)"
+        )
+    else:
+        raise ValueError("Can only process CIFAR10 or CIFAR100")
+
+    classes = dataset.classes
+    num_classes = len(classes)
+
+    train_size = int((1 - ratio) * len(dataset))
+    valid_size = int(ratio * len(dataset))
+
+    train_dataset, valid_dataset = torch.utils.data.random_split(
+        dataset, [train_size, valid_size]
+    )
+
