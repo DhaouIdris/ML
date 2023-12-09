bc00f9dc90266c9026e7052b955c0c4d65c1689a
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2023-12-09 23:56:46 +0400

    Update main.py

--- CIFAR/main.py
+++ CIFAR/main.py
@@ -1,1 +1,30 @@
+import logging
+import sys
+import os
+import pathlib
+
+import yaml
+import torch
+import torchinfo.torchinfo as torchinfo
+
+import Optim_Loss
+import dataloader
+import utils
+
+
+
+def train(config):
+   
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda") if use_cuda else torch.device("cpu")
+
+    logging.info("= Building the dataloaders")
+    train_lod, valid_lod, inputsize, numclasses, classes = dataloader.get_dataloaders(config, False)
+
+  model = config["model"]["class"]
+
+    epochs = config['nepochs']
+    for _ in range(epochs):
+        train_loss = utils.train(model, loss, optim, device, train_lod)
+        valid_loss = utils.test(model, loss, device, valid_lod)
 
