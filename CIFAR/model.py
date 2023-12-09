bc3d675ae2e637e159cddef15880094f14363d42
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2023-12-10 00:12:49 +0400

    test linear

--- CIFAR/model.py
+++ CIFAR/model.py
@@ -17,3 +17,19 @@ def linear(config, input_size, output_size):
         nn.Linear(reduce(operator.mul, input_size, 1), output_size),
     ]
     return nn.Sequential(*layers)
+
+###  test wether the model is working or not
+def test_linear():
+    cfg = {"class": "Linear"}
+    batch_size = 64
+    input_size = (3, 32, 32)
+    num_classes = 18
+    model = linear(cfg, input_size, num_classes)
+
+    input_tensor = torch.randn(batch_size, *input_size)
+    output = model(input_tensor)
+    print(f"Output tensor of size : {output.shape}")
+
+
+if __name__ == "__main__":
+    test_linear()
