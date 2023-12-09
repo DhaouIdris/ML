d09051efc140217a1022f9a5abaf74e8b2fa7668
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2023-12-09 23:58:27 +0400

    linear model added

--- CIFAR/model.py
+++ CIFAR/model.py
@@ -1,1 +1,19 @@
+import torch
+import torch.nn as nn
+from functools import reduce
+import operator
 
+
+def linear(config, input_size, output_size):
+    """
+    congig      -- Configuration parameters from the yaml file
+    input_size  -- tuple
+    output_size -- int
+
+    """
+
+    layers = [
+        nn.Flatten(start_dim=1),
+        nn.Linear(reduce(operator.mul, input_size, 1), output_size),
+    ]
+    return nn.Sequential(*layers)
