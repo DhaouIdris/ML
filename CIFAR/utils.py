e6c40aa225dab5504713877cf2ee0bc9de5e1eb0
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2023-12-12 00:41:22 +0400

    Update utils.py cm

--- CIFAR/utils.py
+++ CIFAR/utils.py
@@ -1,6 +1,10 @@
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import pandas as pd
 
 
 class ModelCheckpoint:
@@ -25,4 +29,32 @@ def generate_unique_logpath(logdir, name):
         if not os.path.isdir(log_path):
             return log_path
         i = i + 1
+
+
+
+def plot_confusion_matrix(modell, valid_lod, classes):
+    y_pred = []
+    y_true = []
+
+    # iterate over test data
+    for inputs, labels in valid_lod:
+        output = modell(inputs)  # Feed Network
+
+        output = (torch.max(torch.exp(output), 1)[1]).data.cpu().numpy()
+        y_pred.extend(output)  # Save Prediction
+
+        labels = labels.data.cpu().numpy()
+        y_true.extend(labels)  # Save Truth
+
+    # Build confusion matrix
+    cf_matrix = confusion_matrix(y_true, y_pred)
+    df_cm = pd.DataFrame(
+        cf_matrix / np.sum(cf_matrix, axis=1)[:, None],
+        index=[i for i in classes],
+        columns=[i for i in classes],
+    )
+    plt.figure(figsize=(12, 7))
+    sns.heatmap(df_cm, annot=True)
+    plt.savefig("confusion_matrix.png")
+
 
