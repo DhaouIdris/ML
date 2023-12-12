19465292fd61a361249d79872c0e774add09ce3d
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2023-12-13 00:24:21 +0400

    Update main.py

--- CIFAR/main.py
+++ CIFAR/main.py
@@ -15,16 +15,30 @@ import utils
 
 def train(config):
    
-    use_cuda = torch.cuda.is_available()
-    device = torch.device("cuda") if use_cuda else torch.device("cpu")
+   use_cuda = torch.cuda.is_available()
+   device = torch.device("cuda") if use_cuda else torch.device("cpu")
 
-    logging.info("= Building the dataloaders")
-    train_lod, valid_lod, inputsize, numclasses, classes = dataloader.get_dataloaders(config, False)
+   logging.info("= Building the dataloaders")
+   train_lod, valid_lod, inputsize, numclasses, classes = dataloader.get_dataloaders(config, False)
+   model = config["model"]["class"]
 
-  model = config["model"]["class"]
+   epochs = config['nepochs']
+      for _ in range(epochs):
+         train_loss = utils.train(model, loss, optim, device, train_lod)
+         valid_loss = utils.test(model, loss, device, valid_lod)
 
-    epochs = config['nepochs']
-    for _ in range(epochs):
-        train_loss = utils.train(model, loss, optim, device, train_lod)
-        valid_loss = utils.test(model, loss, device, valid_lod)
+
+       # creer un datalaoder a parti du fichier config
+   logging.info("= Building the dataloaders")
+   (
+        train_lod,
+        valid_lod,
+        inputsize,
+        numclasses,
+        classes,
+        _,
+        _,
+        _,
+   ) = dataloader.get_dataloaders(config, False)
+
 
