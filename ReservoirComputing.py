24c458b7cee6626631d143725ab076d149fe6666
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2024-01-01 22:59:45 +0400

    Update ReservoirComputing.py forced pendulum

--- ReservoirComputing.py
+++ ReservoirComputing.py
@@ -1,1 +1,7 @@
+from rctorch import *
+import torch
 
+fp_data = data.load("forced_pendulum", train_proportion = 0.2)
+
+force_train, force_test = fp_data["force"]
+target_train, target_test = fp_data["target"]
