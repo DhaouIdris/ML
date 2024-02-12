f1e9b4564a9c2c3c756e7c2535ec733d6940ba67
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2024-02-12 20:40:48 +0400

    Update minESN.py

--- minESN.py
+++ minESN.py
@@ -30,3 +30,19 @@ rc = RC(input_size=state_space, reservoir_size=128, output_size=action_space)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(rc.parameters(), lr=0.001)
+
+
+# Training
+for episode in range(1000):
+    state = env.reset()
+    total_reward = 0
+    
+    while True:
+        
+        state = torch.tensor(state[0])
+        
+        action_probs = rc(state)
+        action = torch.argmax(action_probs).item()
+        
+        next_state, action, reward, done, _ = env.step(action)
+        total_reward += reward
