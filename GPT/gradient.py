5d8e93a19f8b51da6eba5cc96e00126f21706312
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2024-04-22 20:31:40 +0200

    Update gradient.py

--- GPT/gradient.py
+++ GPT/gradient.py
@@ -4,7 +4,7 @@ class Solution:
     def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:      
         for i in range(iterations):
             init = init - learning_rate*2*init 
-        return round(init*100000)/100000  
+        return round(init, 5)
 
 
 
