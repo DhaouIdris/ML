287ab2a5563a11f4c58965f47bd1f79708f63a35
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2024-04-24 23:51:47 +0200

    Update pytorch_test.py

--- GPT/pytorch_test.py
+++ GPT/pytorch_test.py
@@ -12,7 +12,8 @@ from torchtyping import TensorType
 class Solution:
     def reshape(self, to_reshape: TensorType[float]) -> TensorType[float]:
         # torch.reshape() will be useful - check out the documentation
-        pass
+        L = list(to_reshape.size())
+        return torch.reshape(to_reshape, (L[0]*L[1]//2, 2))
 
     def average(self, to_avg: TensorType[float]) -> TensorType[float]:
         # torch.mean() will be useful - check out the documentation
