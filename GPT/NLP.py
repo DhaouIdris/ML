30280ee3d6c4088468ae0d4ce31a5f1e955af387
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2024-05-13 23:47:58 +0200

    Update NLP.py

--- GPT/NLP.py
+++ GPT/NLP.py
@@ -1,1 +1,8 @@
+import torch
+import torch.nn as nn
+from torchtyping import TensorType
 
+# torch.tensor(python_list) returns a Python list as a tensor
+class Solution:
+    def get_dataset(self, positive: List[str], negative: List[str]) -> TensorType[float]:
+        pass
