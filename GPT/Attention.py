773a137afa2e62eff69c18068f24e44d4a1df8d8
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2024-05-28 22:59:43 +0200

    Update Attention.py

--- GPT/Attention.py
+++ GPT/Attention.py
@@ -1,1 +1,25 @@
+import torch
+import torch.nn as nn
+from torchtyping import TensorType
 
+# 0. Instantiate the linear layers in the following order: Key, Query, Value.
+# 1. Biases are not used in Attention, so for all 3 nn.Linear() instances, pass in bias=False.
+# 2. torch.transpose(tensor, 1, 2) returns a B x T x A tensor as a B x A x T tensor.
+# 3. This function is useful:
+#    https://pytorch.org/docs/stable/generated/torch.nn.functional.softmax.html
+# 4. Apply the masking to the TxT scores BEFORE calling softmax() so that the future
+#    tokens don't get factored in at all.
+#    To do this, set the "future" indices to float('-inf') since e^(-infinity) is 0.
+# 5. To implement masking, note that in PyTorch, tensor == 0 returns a same-shape tensor 
+#    of booleans. Also look into utilizing torch.ones(), torch.tril(), and tensor.masked_fill(),
+#    in that order.
+class SingleHeadAttention(nn.Module):
+    
+    def __init__(self, embedding_dim: int, attention_dim: int):
+        super().__init__()
+        torch.manual_seed(0)
+        pass
+    
+    def forward(self, embedded: TensorType[float]) -> TensorType[float]:
+        # Return your answer to 4 decimal places
+        pass
