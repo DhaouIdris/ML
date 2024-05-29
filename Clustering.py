5b2291ca4a0cc8b1376386c41f5df65edbaba342
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2024-05-29 23:02:27 +0200

    Update Clustering.py

--- Clustering.py
+++ Clustering.py
@@ -14,5 +14,14 @@ kmeans = KMeans(n_clusters=n_clusters, random_state=random_state)
 #Training
 kmeans.fit(X)
 
-
+#Prediction
 y_pred = kmeans.predict(X)
+
+#Results
+plt.scatter(X[:, 0], X[:, 1], c=y_pred, s=50, cmap='viridis')
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:, 0], centers[:, 1], c='red', s=200, alpha=0.75)
+plt.title('Résultats du clustering K-Means')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.show()
