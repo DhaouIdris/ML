d1a0bdf8040cf9a5e395491146aadda2d25f0fad
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2024-05-29 23:01:07 +0200

    Update Clustering.py

--- Clustering.py
+++ Clustering.py
@@ -1,1 +1,9 @@
+import numpy as np
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
+
+n_samples = 1500
+random_state = 170
+X, y = make_blobs(n_samples, random_state)
