c5ed53e0464019d69dce77844f67fd419819191b
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2024-05-29 23:01:46 +0200

    Update Clustering.py

--- Clustering.py
+++ Clustering.py
@@ -7,3 +7,12 @@ import matplotlib.pyplot as plt
 n_samples = 1500
 random_state = 170
 X, y = make_blobs(n_samples, random_state)
+
+n_clusters = 3
+kmeans = KMeans(n_clusters=n_clusters, random_state=random_state)
+
+#Training
+kmeans.fit(X)
+
+
+y_pred = kmeans.predict(X)
