d3ab493918d970cf039860a1e4e6265eb396802b
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2024-05-31 23:06:19 +0200

    Update PCA_test.py

--- PCA_test.py
+++ PCA_test.py
@@ -1,7 +1,7 @@
  import numpy as np
 from sklearn.datasets import load_digits
 from sklearn.decomposition import PCA
-
+import matplotlib.pyplot as plt
 
 # Digits data set loading 
 digits = load_digits()
@@ -13,6 +13,22 @@ target_names = digits.target_names
 pca = PCA(n_components=2)
 X_r = pca.fit_transform(X)
 
+
+# Variance for each component
+print(f"Variance for each principal component: {pca.explained_variance_ratio_}")
+
+# 2D vizualisation
+plt.figure(figsize=(10, 8))
+colors = plt.cm.get_cmap('tab10', 10) 
+
+for i in range(len(target_names)):
+    plt.scatter(X_r[y == i, 0], X_r[y == i, 1], color=colors(i), alpha=0.8, label=f'Digit {i}')
+
+plt.legend(loc='best', shadow=False, scatterpoints=1)
+plt.title('PCA of Digits dataset')
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.show()
 
 
 
