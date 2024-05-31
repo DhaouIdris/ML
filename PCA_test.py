4210de5301ff642c2fddbe347f28bf5c2047a573
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2024-05-31 23:04:18 +0200

    Update PCA_test.py

--- PCA_test.py
+++ PCA_test.py
@@ -1,1 +1,18 @@
+ import numpy as np
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+
+
+# Digits data set loading 
+digits = load_digits()
+X = digits.data
+y = digits.target
+target_names = digits.target_names
+
+# initializing pca 
+pca = PCA(n_components=2)
+X_r = pca.fit_transform(X)
+
+
+
 
