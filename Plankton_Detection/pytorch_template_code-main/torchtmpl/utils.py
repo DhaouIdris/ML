5a020cacc7e881936af8a65307baa093ec947080
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2025-03-21 22:58:47 +0100

    Update utils.py

--- Plankton_Detection/pytorch_template_code-main/torchtmpl/utils.py
+++ Plankton_Detection/pytorch_template_code-main/torchtmpl/utils.py
@@ -61,7 +61,7 @@ class ModelCheckpoint(object):
         return False
 
 
-def train(model, loader, f_loss, optimizer, device, dynamic_display=True):
+def train(model, loader, f_loss, optimizer, device, config, dynamic_display=True):
     """
     Train a model for one epoch, iterating over the loader
     using the f_loss to compute the loss and the optimizer
@@ -79,30 +79,43 @@ def train(model, loader, f_loss, optimizer, device, dynamic_display=True):
     # We enter train mode.
     # This is important for layers such as dropout, batchnorm, ...
     model.train()
-
+    scaler = torch.GradScaler()
+    accumulation_steps = 2
     total_loss = 0
+    total_metrics = {
+        "precision": 0,
+        "recall": 0,
+        "f1": 0,
+        "iou": 0,
+    }
     num_samples = 0
     for i, (inputs, targets) in (pbar := tqdm.tqdm(enumerate(loader))):
 
         inputs, targets = inputs.to(device), targets.to(device)
-
-        # Compute the forward propagation
-        outputs = model(inputs)
-
-        loss = f_loss(outputs, targets)
+        targets = targets.unsqueeze(1)
+        with torch.autocast(device_type="cuda", dtype=torch.float16):
+            # Compute the forward propagation
+            outputs = model(inputs)
+            loss = f_loss(outputs, targets) / accumulation_steps
 
         # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
+        scaler.scale(loss).backward()
+        if (i + 1) % accumulation_steps == 0 or (i + 1) == len(loader):
+            scaler.step(optimizer)
+            scaler.update()
+            optimizer.zero_grad(set_to_none=True)
+            
         # Update the metrics
         # We here consider the loss is batch normalized
+        train_metrics = metrics.compute_metrics(y_true=targets, y_pred=(torch.sigmoid(outputs) > config['model']['threshold']).int())
+        for k in total_metrics:
+            total_metrics[k] += inputs.shape[0] * train_metrics[k]
         total_loss += inputs.shape[0] * loss.item()
         num_samples += inputs.shape[0]
-        pbar.set_description(f"Train loss : {total_loss/num_samples:.2f}")
+        pbar.set_description(f"Train loss : {total_loss/num_samples:.2f}, F1-score : {total_metrics['f1']/num_samples:.5f}")
+        
+    return total_loss / num_samples, {k: v / num_samples for k, v in total_metrics.items()}
 
-    return total_loss / num_samples
 
 
 def test(model, loader, f_loss, device):
