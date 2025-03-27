c7fbccf8c0f497a76b8a12f73db110a81626cee8
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2025-03-27 09:23:15 +0100

    Update cnn_models.py

--- Plankton_Detection/pytorch_template_code-main/torchtmpl/models/cnn_models.py
+++ Plankton_Detection/pytorch_template_code-main/torchtmpl/models/cnn_models.py
@@ -7,6 +7,7 @@ import operator
 # External imports
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class AttentionGate(nn.Module):
     """Attention Gate for UNet."""
@@ -62,5 +63,108 @@ def VanillaCNN(cfg, input_size, num_classes):
     num_features = reduce(operator.mul, out_cnn.shape[1:], 1)
     out_layers = [nn.Flatten(start_dim=1), nn.Linear(num_features, num_classes)]
     return nn.Sequential(conv_model, *out_layers)
+
+
+class UNet(nn.Module):
+    def __init__(self, cfg, in_channels, out_channels):
+        super(UNet, self).__init__()
+        
+        # Encoder
+        self.enc1 = self.conv_block(in_channels, 64)
+        self.pool1 = nn.MaxPool2d(2)
+
+        self.enc2 = self.conv_block(64, 128)
+        self.pool2 = nn.MaxPool2d(2)
+
+        self.enc3 = self.conv_block(128, 256)
+        self.pool3 = nn.MaxPool2d(2)
+
+        self.enc4 = self.conv_block(256, 512)
+        self.pool4 = nn.MaxPool2d(2)
+
+        # Bottleneck
+        self.bottleneck = self.conv_block(512, 1024)
+
+        # Decoder with Attention Gates
+        self.upconv4 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.att4 = AttentionGate(512, 512)
+        self.dec4 = self.conv_block(1024, 512)
+
+        self.upconv3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.att3 = AttentionGate(256, 256)
+        self.dec3 = self.conv_block(512, 256)
+
+        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.att2 = AttentionGate(128, 128)
+        self.dec2 = self.conv_block(256, 128)
+
+        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.att1 = AttentionGate(64, 64)
+        self.dec1 = self.conv_block(128, 64)
+
+        # Output
+        self.out_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+
+        # Initialize weights
+        self._initialize_weights()
+
+    def forward(self, x):
+        # Encoder
+        enc1 = self.enc1(x)
+        enc2 = self.enc2(self.pool1(enc1))
+        enc3 = self.enc3(self.pool2(enc2))
+        enc4 = self.enc4(self.pool3(enc3))
+
+        # Bottleneck
+        bottleneck = self.bottleneck(self.pool4(enc4))
+
+        # Decoder with Attention Gates
+        dec4 = self.upconv4(bottleneck)
+        att4 = self.att4(enc4, dec4)
+        dec4 = torch.cat((dec4, att4), dim=1)
+        dec4 = self.dec4(dec4)
+
+        dec3 = self.upconv3(dec4)
+        att3 = self.att3(enc3, dec3)
+        dec3 = torch.cat((dec3, att3), dim=1)
+        dec3 = self.dec3(dec3)
+
+        dec2 = self.upconv2(dec3)
+        att2 = self.att2(enc2, dec2)
+        dec2 = torch.cat((dec2, att2), dim=1)
+        dec2 = self.dec2(dec2)
+
+        dec1 = self.upconv1(dec2)
+        att1 = self.att1(enc1, dec1)
+        dec1 = torch.cat((dec1, att1), dim=1)
+        dec1 = self.dec1(dec1)
+
+        # Output
+        out = self.out_conv(dec1)
+        return out
+
+    @staticmethod
+    def conv_block(in_channels, out_channels):
+        """Convolutional block: 2 convolutions + ReLU + Dropout."""
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(0.5),  # Dropout for regularization
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+    def _initialize_weights(self):
+        """Initialize weights using He initialization."""
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
 
 
