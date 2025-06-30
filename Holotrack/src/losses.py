8ac38daff77b92e3e4dcc3a5bb5860c9fdcbad3f
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2025-06-30 22:42:26 +0200

    focal

--- Holotrack/src/losses.py
+++ Holotrack/src/losses.py
@@ -22,3 +22,40 @@ class BinaryCrossEntropySumsMSE(nn.Module):
         mse_loss = self.mse(probs, targets)
         total_loss = self.alpha * ce_loss + self.beta * mse_loss
         return total_loss
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha1, alpha2, gamma, reduction='mean'):
+        super(FocalLoss, self).__init__()
+        self.alpha1 = alpha1  # Weighting factor for the class
+        self.alpha2 = alpha2  # Weighting factor for the class
+        self.gamma = gamma  # Focusing parameter
+        self.reduction = reduction
+
+    def forward(self, inputs, targets):
+
+        # Calculate the cross entropy loss
+        BCE_loss = nn.BCEWithLogitsLoss(reduction='none')(inputs, targets)
+        
+        # Apply sigmoid to logits to get probabilities
+        inputs = torch.sigmoid(inputs)
+        
+        # Calculate p_t
+        p_t = targets * inputs + (1 - targets) * (1 - inputs)
+        
+        focal_weight = (1 - p_t) ** self.gamma
+
+        if self.alpha1 is not None and self.alpha2 is not None:
+            alpha_factor = targets * self.alpha2 + (1 - targets) * self.alpha1
+            focal_weight = alpha_factor * focal_weight
+
+        # Apply focal weight to loss
+        loss = focal_weight * BCE_loss
+        
+        # Apply reduction method
+        if self.reduction == 'mean':
+            return loss.mean()
+        elif self.reduction == 'sum':
+            return loss.sum()
+        else:
+            return loss  # Return per-element loss if no reduction
