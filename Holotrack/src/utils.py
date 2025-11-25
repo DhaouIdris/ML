08b628b6eed3497d15f59cfcf513ee78585bdf5d
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2025-11-25 15:45:46 +0100

    angle visu

--- Holotrack/src/utils.py
+++ Holotrack/src/utils.py
@@ -1,3 +1,11 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+
+def visualize_angle(otf3d: np.ndarray):
+    phase = np.angle(np.fft.fftshift(otf3d))
+    fig, axes = plt.subplots(1,3, figsize=(19.2,10.8))
+    axes = axes.ravel()
+    for i in range(3):
+        axes[i].imshow(phase[:, :, i], cmap="gray")
+    plt.show()
