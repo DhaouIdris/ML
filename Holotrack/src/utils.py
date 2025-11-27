f1160f5356974d24859567c142d0c52740b1c309
DhaouIdris <128693528+DhaouIdris@users.noreply.github.com>
2025-11-27 20:48:54 +0100

    utils update

--- Holotrack/src/utils.py
+++ Holotrack/src/utils.py
@@ -22,3 +22,39 @@ def get_logdir(logdir):
     # TODO: add time
     timestamp = datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
     return logdir+"--"+timestamp
+
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+def get_summary(trainer):
+    # TODO: use torchinfo fro model summary
+    summary = (f"## Logdir: {trainer.logdir}\n\n" + "## Dataset:\n" +
+        str(getattr(trainer, "data", None)) + "\n\n" + "## Model:\n" + str(getattr(trainer, "model", None)) +
+        "## Loss:\n" + str(getattr(trainer, "loss", None)) + "\n\n" +
+        "## Optimizer:\n" + str(getattr(trainer, "optimizer", None)) +
+        "## Learning rate scheduler:\n" + str(getattr(trainer, "lr_scheduler", None)) +
+        "## Metrics:\n" + str(getattr(trainer, "metric_funcs", None)) +
+        f"\n\n## Seed: {trainer.config['seed']}" if "seed" in trainer.config else "" +
+        f"\n\n## Epochs: {trainer.config['epochs']}"
+
+        # "## Model architecture\n" + f"{trainer.model.__str__}\n\n"
+    )
+
+    with open(os.path.join(trainer.logdir, "summary.txt"), 'w') as f:
+        f.write(summary)
+
+    if hasattr(trainer, "neptune_run"):
+        trainer.neptune_run["summary"] = summary
+
+def save_figures(trainer):
+    for name, results in trainer.results.items():
+        plt.plot([r["train"] for r in results], label="train")
+        plt.plot([r["eval"] for r in results], label="eval")
+        plt.title(name)
+        plt.savefig(os.path.join(trainer.logdir, f"{name}-train-vs-eval.jpg"))
+        plt.legend()
+        plt.close()
